41f0401e5f6aa8808f6b34542d593ee281433ce6
Andrii Shandryk <shandrykjj512@gmail.com>
2025-08-22 01:17:55 +0300

    Add RabbitMQ and Redis support to the host application

--- src/aspire/PizzaShop.AppHost/AppHost.cs
+++ src/aspire/PizzaShop.AppHost/AppHost.cs
@@ -1,3 +1,7 @@
 ﻿IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+IResourceBuilder<RedisResource> redis = builder.AddRedis("redis");
+IResourceBuilder<RabbitMQServerResource> rabbitMq = builder.AddRabbitMQ("eventbus")
+    .WithLifetime(ContainerLifetime.Persistent);
+
 
 await builder.Build().RunAsync().ConfigureAwait(false);
