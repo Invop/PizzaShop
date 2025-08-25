3689919d0af37d56d671594ba2a22b6fd9e8b240
Andrii Shandryk <shandrykjj512@gmail.com>
2025-08-25 19:02:03 +0300

    feat(api): rename Base constant and add Catalog structure
    * Rename the `Base` constant in the `ApiEndpoints` class to `ApiBase`.
    * Introduce a new nested static class `Catalog` to organize API endpoints.
    * Add nested static classes: `Items`, `Categories`, `Toppings`, and `Preparations`.
    * Each nested class defines a `Base` constant for constructing URL paths.
    * Implement constants for CRUD operations in each class using their respective `Base` constants.

--- src/aspire/PizzaShop.ServiceDefaults/ApiEndpoints.cs
+++ src/aspire/PizzaShop.ServiceDefaults/ApiEndpoints.cs
@@ -1,5 +1,54 @@
 ﻿namespace PizzaShop.ServiceDefaults;
 public static class ApiEndpoints
 {
-    public const string Base = "api/v{version:apiVersion}";
+    public const string ApiBase = "api/v{version:apiVersion}";
+
+    public static class Catalog
+    {
+        public const string Base = $"{ApiBase}/catalog";
+        public static class Items
+        {
+            public const string Base = $"{Catalog.Base}/items";
+
+            public const string Create = Base;
+            public const string Get = $"{Base}/{{idOrSlug}}";
+            public const string GetAll = Base;
+            public const string Update = $"{Base}/{{id:guid}}";
+            public const string Delete = $"{Base}/{{id:guid}}";
+        }
+
+        public static class Categories
+        {
+            public const string Base = $"{Catalog.Base}/categories";
+
+            public const string Create = Base;
+            public const string Get = $"{Base}/{{idOrSlug}}";
+            public const string GetAll = Base;
+            public const string Update = $"{Base}/{{id:guid}}";
+            public const string Delete = $"{Base}/{{id:guid}}";
+        }
+
+        public static class Toppings
+        {
+            public const string Base = $"{Catalog.Base}/toppings";
+
+            public const string Create = Base;
+            public const string Get = $"{Base}/{{id:guid}}";
+            public const string GetAll = Base;
+            public const string Update = $"{Base}/{{id:guid}}";
+            public const string Delete = $"{Base}/{{id:guid}}";
+        }
+
+        public static class Preparations
+        {
+            public const string Base = $"{Catalog.Base}/preparations";
+
+            public const string Create = Base;
+            public const string Get = $"{Base}/{{id:guid}}";
+            public const string GetAll = Base;
+            public const string Update = $"{Base}/{{id:guid}}";
+            public const string Delete = $"{Base}/{{id:guid}}";
+        }
+
+    }
 }
